2f50a91db72d4be9461f2d8b29225802ebc0c4ca
surender <surenderthakur40437@gmail.com>
2026-01-12 03:00:34 +0530

    i

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,6 +1,6 @@
 import appointment_img from './appointment_img.png'
 import banner_doctors from './banner_doctors.png'
-import header_img from './header_img.png'
+import header_img from './header_img.jpg'
 import group_profiles from './group_profiles.png'
 import profile_pic from './profile_pic.png'
 import contact_image from './contact_image.png'
