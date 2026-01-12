dc931c2f39b5ff7057609bcd4a573566bbeab675
surender <surenderthakur40437@gmail.com>
2026-01-12 19:47:50 +0530

    li

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -8,7 +8,7 @@ const Contact = () => {
         <p>CONTACT <span className='text-gray-700 font-semibold'>US</span> </p>
       </div>
       <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28 text-sm'>
-        <img className='w-full md:max-w-[360px]' src={assets.contact_image} alt="" />
+        <img className='w-full md:max-w-[360px] h-auto object-cover rounded-lg' src={assets.contact_image} alt="" />
         <div className='flex flex-col justify-center items-start gap-6'>
           <p className='font-semibold text-lg text-gray-600'>Our Office</p>
           <p className='text-gray-500'>54709 william Station <br />Suit 350, Washington, USA</p>
